c5b71227726e5c15828f26be2e3a7512da3c55cf
itemiral <itemiral@ucsc.edu>
2021-07-15 02:32:04 -0700

    adding scene structure

---
+++ src/scenes/Level2.js
@@ -0,0 +1,5 @@
+class Level2 extends Phaser.Scene {
+    constructor() {
+        super("level2Scene");
+    }
+}
---
+++ src/scenes/Level3.js
@@ -0,0 +1,5 @@
+class Level3 extends Phaser.Scene {
+    constructor() {
+        super("level3Scene");
+    }
+}
